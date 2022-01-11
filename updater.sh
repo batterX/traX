bdf666172b00a1d0a6f2b4632e1556098e664e87
batterX <ivan@visionups.com>
2022-01-11 14:53:48 +0100

    Update updater.sh
    Changed git:// to https://

--- updater.sh
+++ updater.sh
@@ -3,7 +3,7 @@
 
 cd /home/pi
 
-git clone git://github.com/batterx/trax.git
+git clone https://github.com/batterx/trax.git
 
 sudo cp /home/pi/trax/update.sh /home/pi
 sudo chmod 777 /home/pi/update.sh
